54d920a59d96e47a80db5d707e3c71b386207ad8
Aaron Belay <AaronBelay77@gmail.com>
2024-08-17 22:54:08 -0400

    Refactor: Separate player and team stats, fix module imports, consolidate code into upsortsdata.js
    - Separated player and team stats for better organization.
    - Fixed node_modules import issue.
    - Consolidated all code into a single main file: upsortsdata.js.

---
+++ playerstats/playerstats_settings.js
@@ -0,0 +1,76 @@
+const playerDataFields = [
+
+    // Offensive
+    "games_played",
+    "games_started",
+
+    "minutes_played",
+    "field_goal_made",
+    "field_goal_percentage",
+    "three_pointers_made",
+    "three_pointers_percentage",
+    "free_throws_made",
+    "free_throws_percentage",
+    "total_points",
+
+    // Ball Control (Defensive)
+    "offensive_rebounds",
+    "defensive_rebounds",
+    "total_rebounds",
+    "assists",
+    "turnovers",
+    "steals",
+    "blocks",
+
+    // Ball Control Extended
+    "personal_fouls",
+    "disqualifications",
+    "assist_to_turnover_ratio",
+    "team"
+];
+  
+  // Mapping of team names to their respective conferences
+const sortCategories = [
+    
+    "gp",
+    "gs",
+    "min",
+    "pts",
+    "fgp",
+    "fgpt",
+    "fgp3",
+    "fgpt3",
+    "ftp",
+    "ftpt",
+    "oreb",
+    "dreb",
+    "treb",
+    "ast",
+    "to",
+    "stl",
+    "blk",
+    "pf",
+    "dq",
+    "ato"
+  
+];
+
+
+  // Mapping of stat group names to their respective dropdown values and number of stats
+const playerStatGroups = {
+    "Offense": { number_of_stats: 8 },
+    "Defense": { number_of_stats: 7 },
+    "Ball Control": { number_of_stats: 3 },
+};
+
+
+// Convert the keys from the statGroups object to an array for easier iteration
+const playerStatGroupKeys = Object.keys(playerStatGroups);
+
+// Export all constants and mappings at once
+module.exports = {
+    playerDataFields,
+    sortCategories,
+    playerStatGroups,
+    playerStatGroupKeys
+};
